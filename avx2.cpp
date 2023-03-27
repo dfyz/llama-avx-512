827196d22c8900768737961be920a6738f1c6c0c
Ivan Komarov <Ivan.Komarov@dfyz.info>
2023-03-27 03:16:45 +0200

    https://gist.github.com/Const-me/4d30e1fc767ab314596e16e90f53b6f4

--- avx2.cpp
+++ avx2.cpp
@@ -134,32 +134,27 @@ inline static void ggml_vec_dot_q4_0(const int n, float * __restrict__ s, const 
         const float * d0_0 = (const float *) (pd0 + i*bs);
         const float * d1_0 = (const float *) (pd1 + i*bs);
 
-        const uint8_t * __restrict__ p0 = pb0 + i*bs;
-        const uint8_t * __restrict__ p1 = pb1 + i*bs;
+        const uint8_t * __restrict__ ptr0 = pb0 + i*bs;
+        const uint8_t * __restrict__ ptr1 = pb1 + i*bs;
 
         // Compute combined scale for the block
         const __m256 scale = _mm256_mul_ps( _mm256_broadcast_ss( d0_0 ), _mm256_broadcast_ss( d1_0 ) );
 
         // Load 16 bytes, and unpack 4 bit fields into bytes, making 32 bytes
-        __m256i bx = bytesFromNibbles( p0 );
-        __m256i by = bytesFromNibbles( p1 );
+        __m256i bx = bytesFromNibbles( ptr0 );
+        __m256i by = bytesFromNibbles( ptr1 );
 
-        // Now we have a vector with bytes in [ 0 .. 15 ] interval. Offset them into [ -8 .. +7 ] interval.
         const __m256i off = _mm256_set1_epi8( 8 );
-        bx = _mm256_sub_epi8( bx, off );
         by = _mm256_sub_epi8( by, off );
+        // These weird multiplication instructions compute a0*b0 + a1*b1 for uint8_t a, int8_t b
+        __m256i p1 = _mm256_maddubs_epi16( bx, by );
+        __m256i p2 = _mm256_maddubs_epi16( off, by );
+        __m256i p16 = _mm256_sub_epi16( p1, p2 );
 
-        // Sign-extend first 16 signed bytes into int16_t
-        __m256i x16 = _mm256_cvtepi8_epi16( _mm256_castsi256_si128( bx ) );
-        __m256i y16 = _mm256_cvtepi8_epi16( _mm256_castsi256_si128( by ) );
-        // Compute products of int16_t integers, add pairwise
-        __m256i i32 = _mm256_madd_epi16( x16, y16 );
-
-        // Sign-extend last 16 signed bytes into int16_t vectors
-        x16 = _mm256_cvtepi8_epi16( _mm256_extracti128_si256( bx, 1 ) );
-        y16 = _mm256_cvtepi8_epi16( _mm256_extracti128_si256( by, 1 ) );
-        // Accumulate products of int16_t integers
-        i32 = _mm256_add_epi32( i32, _mm256_madd_epi16( x16, y16 ) );
+        // Competes for the same ports as _mm256_maddubs_epi16, needs the constant vector with ones,
+        // and takes 3-5 cycles of latency
+        // However, that's 1 instruction instead of 3.
+        __m256i i32 = _mm256_madd_epi16( p16, _mm256_set1_epi16( 1 ) );
 
         // Convert int32_t to float
         __m256 p = _mm256_cvtepi32_ps( i32 );
