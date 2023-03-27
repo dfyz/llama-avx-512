3f34046565c20d16637a4bdf9479a310477b000b
Ivan Komarov <Ivan.Komarov@dfyz.info>
2023-03-28 01:29:35 +0200

    doesn't work but okay

--- my_avx512.cpp
+++ my_avx512.cpp
@@ -153,26 +153,28 @@ static inline __m512 dot_q4_0_oneblock_avx512(
 }
 
 inline static __m512 blk_2_scales(__m512i x) {
-    const __m512i prm = _mm512_setr_epi32(
-        0, 0, 0, 0,
-        0, 0, 0, 0,
+    const __m512i prm = _mm512_set_epi32(
         5, 5, 5, 5,
-        5, 5, 5, 5
+        5, 5, 5, 5,
+        0, 0, 0, 0,
+        0, 0, 0, 0
     );
     return _mm512_permutexvar_ps(prm, (__m512)x);
 }
 
 inline static __m512i blk_2_bytes(__m512i x) {
     const __m512i prm = _mm512_set_epi8(
-        4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11,
-        12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19,
-        24, 24, 25, 25, 26, 26, 27, 27, 28, 28, 29, 29, 30, 30, 31, 31,
-        32, 32, 33, 33, 34, 34, 35, 35, 36, 36, 37, 37, 38, 38, 39, 39
+        39, 39, 38, 38, 37, 37, 36, 36, 35, 35, 34, 34, 33, 33, 32, 32,
+        31, 31, 30, 30, 29, 29, 28, 28, 27, 27, 26, 26, 25, 25, 24, 24,
+        19, 19, 18, 18, 17, 17, 16, 16, 15, 15, 14, 14, 13, 13, 12, 12,
+        11, 11, 10, 10, 9, 9, 8, 8, 7, 7, 6, 6, 5, 5, 4, 4
     );
-
     const __m512i y = _mm512_permutexvar_epi8(prm, x);
 
-    return _mm512_setzero_epi32();
+    const __m512i prm2 = _mm512_set1_epi64(0x3c302c201c100c00);
+    const __m512i z = _mm512_multishift_epi64_epi8(prm2, y);
+
+    return _mm512_and_si512(_mm512_set1_epi8(0xF), z);
 }
 
 inline static void ggml_vec_dot_q4_0(const int n, float * __restrict__ s, const void * __restrict__ x, const void * __restrict__ y) {
