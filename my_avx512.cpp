0508e89d7b490a11e90a49a2d932a68a33812a61
Ivan Komarov <Ivan.Komarov@dfyz.info>
2023-03-28 00:29:18 +0200

    first draft

--- my_avx512.cpp
+++ my_avx512.cpp
@@ -152,6 +152,14 @@ static inline __m512 dot_q4_0_oneblock_avx512(
     return _mm512_fmadd_ps( scale, p, acc );
 }
 
+inline static __m512 blk_2_scales(__m512i x) {
+    return _mm512_setzero_ps();
+}
+
+inline static __m512i blk_2_bytes(__m512i x) {
+    return _mm512_setzero_epi32();
+}
+
 inline static void ggml_vec_dot_q4_0(const int n, float * __restrict__ s, const void * __restrict__ x, const void * __restrict__ y) {
     const int nb = n / QK;
 
@@ -160,43 +168,43 @@ inline static void ggml_vec_dot_q4_0(const int n, float * __restrict__ s, const 
     const uint8_t * __restrict__ pd0 = ((const uint8_t *)x + 0*bs);
     const uint8_t * __restrict__ pd1 = ((const uint8_t *)y + 0*bs);
 
-    const uint8_t * __restrict__ pb0 = ((const uint8_t *)x + 0*bs + sizeof(float));
-    const uint8_t * __restrict__ pb1 = ((const uint8_t *)y + 0*bs + sizeof(float));
-
     float sumf = 0.0;
 
     // Initialize accumulator with zeros
     __m512 acc = _mm512_setzero_ps();
 
-    for (int i = 0; i < nb; ++i) {
-        const float * d0_0 = (const float *) (pd0 + i*bs);
-        const float * d1_0 = (const float *) (pd1 + i*bs);
+    const __mmask64 blk_2_mask = 0xFF'FF'FF'FF'FFUL;
+    const __m512i off = _mm512_set1_epi8(8);
 
-        const uint8_t * __restrict__ p0 = pb0 + (i+0)*bs;
-        const uint8_t * __restrict__ p1 = pb1 + (i+0)*bs;
+    for (int i = 0; i < nb; i += 2) {
+        __m512i blk0 = _mm512_maskz_loadu_epi8(blk_2_mask, pd0 + 2*i*bs);
+        __m512i blk1 = _mm512_maskz_loadu_epi8(blk_2_mask, pd1 + 2*i*bs);
 
-        // Compute combined scale for the block
-        float scaleScalar = d0_0[0] * d1_0[0];
-        __m512 scale = _mm512_set1_ps( scaleScalar );
+        const __m512 scales = _mm512_mul_ps(
+            blk_2_scales(blk0),
+            blk_2_scales(blk1)
+        );
 
-        __m256i bx = bytesFromNibbles( p0 );
-        __m256i by = bytesFromNibbles( p1 );
+        const __m512i bx = blk_2_bytes(blk0);
+        __m512i by = blk_2_bytes(blk1);
+        by = _mm512_sub_epi8(by, off);
 
-        // Now we have a vector with bytes in [ 0 .. 15 ] interval. Offset them into [ -8 .. +7 ] interval.
-        const __m256i off = _mm256_set1_epi8( 8 );
-        bx = _mm256_sub_epi8( bx, off );
-        by = _mm256_sub_epi8( by, off );
+        const __m512i aa = _mm512_dpbusds_epi32(
+            _mm512_setzero_epi32(),
+            bx, by
+        );
+        const __m512i bb = _mm512_dpbusds_epi32(
+            _mm512_setzero_epi32(),
+            off, by
+        );
+        const __m512i diff = _mm512_sub_epi32(aa, bb);
 
-        // Sign-extend 16 signed bytes into int16_t
-        __m512i x32 = _mm512_cvtepi8_epi16( bx );
-        __m512i y32 = _mm512_cvtepi8_epi16( by );
-        // Compute products of int16_t integers, add pairwise
-        __m512i i64 = _mm512_madd_epi16( x32, y32 );
 
-        // Convert int32_t to float
-        __m512 p = _mm512_cvtepi32_ps( i64 );
-        // Apply the scale, and accumulate
-        acc = _mm512_fmadd_ps( scale, p, acc );
+        acc = _mm512_fmadd_ps(
+            scales,
+            _mm512_cvtepi32_ps(diff),
+            acc
+        );
     }
 
     // Horizontal sum of all lanes of the accumulator
