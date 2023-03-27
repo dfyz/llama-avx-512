2183e80ca7bdb3d2acbb8fc42ed1c8d95bceb577
Ivan Komarov <Ivan.Komarov@dfyz.info>
2023-03-28 01:12:17 +0200

    better

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-g++ -O3 -g -std=c++20 -DNDEBUG -mavx2 -mavx512f -mavx512bw -mfma -mavx512vnni -mavx512vl -o main \
+g++ -O3 -g -std=c++20 -DNDEBUG -mavx2 -mavx512f -mavx512bw -mfma -mavx512vnni -mavx512vl -mavx512vbmi -o main \
     main.cpp \
     avx2.cpp \
     avx512.cpp \
