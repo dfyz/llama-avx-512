6c8a77ddc995d2ef81078143f43074e942697f0a
Ivan Komarov <Ivan.Komarov@dfyz.info>
2023-04-01 02:58:50 +0200

    simplify some more

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-g++ -O3 -g -std=c++20 -DNDEBUG -mavx2 -mavx512f -mavx512bw -mfma -mavx512vnni -mavx512vl -mavx512vbmi -o main \
+clang++ -O3 -g -std=c++20 -DNDEBUG -mavx2 -mavx512f -mavx512bw -mfma -mavx512vnni -mavx512vl -mavx512vbmi -o main \
     main.cpp \
     avx512_modern.cpp \
     ggml.cpp
