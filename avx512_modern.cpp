2f348501cead46a37e8cea26b21e1923bc2fe469
Ivan Komarov <Ivan.Komarov@dfyz.info>
2023-04-07 01:29:59 +0200

    one multiplication

--- avx512_modern.cpp
+++ avx512_modern.cpp
@@ -17,7 +17,9 @@ inline static __m512i blk_2_bytes(__m512i x) {
     const __m512i prm2 = _mm512_set1_epi64(0x3c302c201c100c00);
     const __m512i z = _mm512_multishift_epi64_epi8(prm2, y);
 
-    return _mm512_and_si512(_mm512_set1_epi8(0xF), z);
+    const __m512i masked = _mm512_and_si512(_mm512_set1_epi8(0xF), z);
+    const __m512i off = _mm512_set1_epi8(8);
+    return _mm512_sub_epi8(masked, off);
 }
 
 inline static void ggml_vec_dot_q4_0(const int n, float * __restrict__ s, const void * __restrict__ vx, const void * __restrict__ vy) {
@@ -31,7 +33,6 @@ inline static void ggml_vec_dot_q4_0(const int n, float * __restrict__ s, const 
     // Initialize accumulator with zeros
     __m512 acc = _mm512_setzero_ps();
 
-    const __m512i off = _mm512_set1_epi8(8);
     const __mmask16 scale_mul_mask = 0x21;
 
     const __m512i prm = _mm512_set_epi32(
@@ -49,22 +50,20 @@ inline static void ggml_vec_dot_q4_0(const int n, float * __restrict__ s, const 
         scales = _mm512_permutexvar_ps(prm, scales);
 
         const __m512i bx = blk_2_bytes(blk0);
-        __m512i by = blk_2_bytes(blk1);
-        by = _mm512_sub_epi8(by, off);
+        const __m512i by = blk_2_bytes(blk1);
 
-        const __m512i aa = _mm512_dpbusds_epi32(
+        const __mmask64 neg_mask = _mm512_movepi8_mask(bx);
+        const __m512i abs_bx = _mm512_abs_epi8(bx);
+        const __m512i neg_by = _mm512_mask_sub_epi8(by, neg_mask, _mm512_setzero_si512(), by);
+
+        const __m512i prod = _mm512_dpbusds_epi32(
             _mm512_setzero_epi32(),
-            bx, by
+            abs_bx, neg_by
         );
-        const __m512i bb = _mm512_dpbusds_epi32(
-            _mm512_setzero_epi32(),
-            off, by
-        );
-        const __m512i diff = _mm512_sub_epi32(aa, bb);
 
         acc = _mm512_fmadd_ps(
             scales,
-            _mm512_cvtepi32_ps(diff),
+            _mm512_cvtepi32_ps(prod),
             acc
         );
     }
