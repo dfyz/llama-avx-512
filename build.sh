d40d3ee0cfdda0139625933c9f505a3e6b5b215e
Ivan Komarov <Ivan.Komarov@dfyz.info>
2023-04-09 22:21:27 +0200

    flags

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-g++ -O3 -g -std=c++20 -DNDEBUG -mavx2 -mavx512f -mavx512bw -mfma -mavx512vnni -mavx512vl -mavx512vbmi -o main \
+g++ -O3 -g -std=c++20 -DNDEBUG -mfma -mavx2 \
+    -mavx512f -mavx512bw -mavx512vnni -mavx512vbmi \
+    -o main \
     main.cpp \
     avx2.cpp \
     avx2_const_me.cpp \
